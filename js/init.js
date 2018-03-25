4a3e93702d102110635ac1bc1237bb39fa2b7026
emmattfu <makcimdjan@gmail.com>
2018-03-25 19:35:00 +0300

    footer

--- js/init.js
+++ js/init.js
@@ -3,8 +3,9 @@ $( function() {
     $( "#tabs" ).tabs();
     $( "#portfolio-tabs" ).tabs();
 
-    //carousel init
-    $(".reviews-carousel").slick({
-        dots: true,
-    });
+    // init slick carousel 
+    $('.reviews-carousel').slick({      
+        dots: true,      
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',      
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',  });
 });
