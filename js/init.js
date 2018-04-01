2b3560e5f2a2ceaab64c2fedb9563117d3e53324
emmattfu <makcimdjan@gmail.com>
2018-04-01 18:12:23 +0300

    767px

--- js/init.js
+++ js/init.js
@@ -8,4 +8,10 @@ $( function() {
         dots: true,      
         prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',      
         nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',  });
+    
+    // init slick nav 
+    $ ('.header-nav').slicknav({
+        appendTo: '.header .container-fluid',
+        label: "",
+    });
 });
